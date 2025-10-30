c2e3b612164df1d9571b390fa5d735eefca288d4
Xert <xertdev@gmail.com>
2025-10-30 21:09:45 +0100

    Initial theme (light mode only)

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+	description = "Xert's blog";
+
+	inputs = {
+		nixpkgs.url = "github:NixOs/nixpkgs/nixos-25.05";
+		flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs = { nixpkgs-lib.follows = "nixpkgs"; };
+    };
+
+    git-hooks-nix = {
+      url = "github:cachix/git-hooks.nix";
+      inputs = { nixpkgs.follows = "nixpkgs"; };
+    };
+	};
+
+  outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [ inputs.git-hooks-nix.flakeModule ];
+
+      systems = [ "x86_64-linux" ];
+
+      perSystem = { config, system, ... }: {
+        devShells.default = pkgs.mkShellNoCC {
+          buildInputs = with pkgs; [
+            hugo
+          ];
+        };
+      };
+    };
+}
